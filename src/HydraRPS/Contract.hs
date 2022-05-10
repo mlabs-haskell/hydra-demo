33c169808c2fd781741785f15417f4dda3c8c984
Anatoly Zaretsky <anatoly.zaretsky@gmail.com>
2022-05-10 12:55:58 +0300

    Add dummy Plutus contract that at least builds

---
+++ src/HydraRPS/Contract.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+module HydraRPS.Contract (
+    winnerValidator
+  , winnerValidatorHash
+  , winnerValidatorAddress
+) where
+
+import Ledger qualified (ScriptContext, Address, ValidatorHash, scriptHashAddress)
+import Ledger.Typed.Scripts qualified as Scripts
+import PlutusTx qualified
+import PlutusTx.Prelude
+
+type Gesture = ()
+type Redeemer = ()
+
+{-# INLINABLE mkWinnerValidator #-}
+mkWinnerValidator :: Gesture -> Redeemer -> Ledger.ScriptContext -> Bool
+mkWinnerValidator () () _ = True
+
+data RPS
+instance Scripts.ValidatorTypes RPS where
+  type RedeemerType RPS = Redeemer
+  type DatumType RPS = Gesture
+
+typedWinnerValidator :: Scripts.TypedValidator RPS
+typedWinnerValidator = Scripts.mkTypedValidator @RPS
+  $$(PlutusTx.compile [|| mkWinnerValidator ||])
+  $$(PlutusTx.compile [|| wrap ||])
+  where wrap = Scripts.wrapValidator @Gesture @Redeemer
+
+winnerValidator :: Scripts.Validator
+winnerValidator = Scripts.validatorScript typedWinnerValidator
+
+winnerValidatorHash :: Ledger.ValidatorHash
+winnerValidatorHash = Scripts.validatorHash typedWinnerValidator
+
+winnerValidatorAddress :: Ledger.Address
+winnerValidatorAddress = Ledger.scriptHashAddress winnerValidatorHash
