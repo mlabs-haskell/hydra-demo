79561b4d559ed7fd502149852685f12a825dc5b7
Anatoly Zaretsky <anatoly.zaretsky@gmail.com>
2022-05-30 08:30:22 +0300

    Add "abort", "close", and "commit" commands
    Alice:
    commit 3eeea5c2376b033d5bdeab6fe551950883b04c08a37848c6d648ea03476dce83#1 addr_test1vru2drx33ev6dt8gfq245r5k0tmy7ngqe79va69de9dxkrg09c7d3 1000000000
    
    Bob:
    commit bd279aad1fa00d7f5cd00b33ad0ae20ac493f29558809a110761b4d3136324a3#1 addr_test1vqg9ywrpx6e50uam03nlu0ewunh3yrscxmjayurmkp52lfskgkq5k 1000000000
    
    Note: for some reason "close" fails with
    ErrScriptExecutionFailed (RdmrPtr Spend 0,ValidationFailedV1 (CekError An error has occurred:  User error:
    The provided Plutus code called 'error'.) ["fannedOutUtxoHash /= closedUtxoHash","PT5"])

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,21 @@
 {-# LANGUAGE PatternSynonyms #-}
 module Main (main, ClientInput (NewTxCBOR)) where
 
-import Cardano.Api (AlonzoEra, UTxO, Tx, serialiseToCBOR)
+import Cardano.Api (AlonzoEra, AsType (AsAddressInEra, AsAlonzoEra), Lovelace (Lovelace), UTxO (UTxO), Tx, deserialiseAddress, serialiseToCBOR)
 import Control.Concurrent (forkIO, newChan, readChan, writeChan)
 import Control.Exception (SomeException, displayException, fromException, try)
 import Data.Aeson qualified as Aeson (ToJSON, encode, toJSON)
 import Data.Functor (void)
+import Data.Map qualified as Map (singleton)
+import Data.String (fromString)
 import Data.Text (Text)
 import Data.Text.IO qualified as Text (putStrLn)
 import GHC.Generics (Generic)
 import Network.WebSockets (ConnectionException, receiveData, sendTextData, runClient)
 import System.Environment (getArgs)
 import System.IO.Error (isEOFError)
+
+import HydraRPS.Tx (parseTxIn, txOutToAddress)
 
 import Prelude
 
@@ -50,7 +54,12 @@ data AppEvent =
   | UserCommand UserCommand
 
 type ApiEvent = Maybe Text
-data UserCommand = SendInit Int | Exit
+data UserCommand =
+    InitHead Int
+  | CommitToHead (UTxO AlonzoEra)
+  | Exit
+  | AbortHead
+  | CloseHead
 
 apiReader :: IO Text -> (ApiEvent -> IO ()) -> IO ()
 apiReader nextServerEvent enqueue = go
@@ -79,9 +88,17 @@ commandReader enqueue = go
 
         Right command -> case words command of
           ["exit"] -> enqueue Exit
+          ["abort"] -> enqueue AbortHead >> go
+          ["close"] -> enqueue CloseHead >> go
           ["init", str] | [(period, "")] <- reads str -> do
-            enqueue $ SendInit period
+            enqueue $ InitHead period
             go
+          ["commit", txInStr, addrStr, lovelaceStr]
+            | Right txIn <- parseTxIn (fromString txInStr)
+            , Just addr <- deserialiseAddress (AsAddressInEra AsAlonzoEra) (fromString addrStr)
+            , [(lovelace, "")] <- reads lovelaceStr -> do
+              enqueue $ CommitToHead $ UTxO $ Map.singleton txIn (txOutToAddress addr (Lovelace lovelace))
+              go
           cmd -> do
             putStrLn $ "input: unknown command " <> show cmd
             go
@@ -102,6 +119,11 @@ eventProcessor submitCommand nextEvent = go
         UserCommand command ->
           case command of
             Exit -> return ()
-            SendInit period -> do
+            AbortHead -> submitCommand Abort >> go
+            CloseHead -> submitCommand Close >> go
+            InitHead period -> do
               submitCommand $ Init period
+              go
+            CommitToHead utxoToCommit -> do
+              submitCommand $ Commit utxoToCommit
               go
