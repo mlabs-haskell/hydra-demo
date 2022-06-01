ad4c84928e1a572ccf09e5d6462e96769313c049
Anatoly Zaretsky <anatoly.zaretsky@gmail.com>
2022-06-01 16:16:58 +0300

    Add `bet` command implementation

--- app/Main.hs
+++ app/Main.hs
@@ -92,7 +92,7 @@ data UserCommand
   | AbortHead
   | CloseHead
   | IssueFanout
-  | Bet TxIn UserInput.PlayParams
+  | Bet TxIn Lovelace UserInput.PlayParams
   | Claim TxIn UserInput.ClaimParams
 
 data UserCredentials = UserCredentials
@@ -148,13 +148,14 @@ commandReader enqueue = go
           ["commit", txInStr, addrStr, lovelaceStr]
             | Right txIn <- parseTxIn (fromString txInStr)
               , Just addr <- deserialiseAddress (AsAddressInEra AsAlonzoEra) (fromString addrStr)
-              , [(lovelace, "")] <- reads lovelaceStr -> do
-              enqueue $ CommitToHead $ UTxO $ Map.singleton txIn (txOutToAddress addr (Lovelace lovelace))
+              , Just lovelace <- parseLovelace lovelaceStr -> do
+              enqueue $ CommitToHead $ UTxO $ Map.singleton txIn (txOutToAddress addr lovelace)
               go
-          "bet" : txInStr : ppStr
+          "bet" : txInStr : txInLovelaceStr : ppStr
             | Right txIn <- parseTxIn (fromString txInStr)
+              , Just txInLovelace <- parseLovelace txInLovelaceStr
               , Just pp <- parseJsonArgs ppStr -> do
-              enqueue (Bet txIn pp)
+              enqueue (Bet txIn txInLovelace pp)
               go
           "claim" : collateralTxInStr : cpStr
             | Right collateralTxIn <- parseTxIn (fromString collateralTxInStr)
@@ -166,6 +167,11 @@ commandReader enqueue = go
 
     parseJsonArgs :: Aeson.FromJSON a => [String] -> Maybe a
     parseJsonArgs = Aeson.decodeStrict . encodeUtf8 . fromString . unwords
+
+    parseLovelace :: String -> Maybe Lovelace
+    parseLovelace str
+      | [(lovelace, "")] <- reads str = Just (Lovelace lovelace)
+      | otherwise = Nothing
 
 eventProcessor :: forall (m :: Type -> Type). (MonadIO m, MonadReader HeadState m) => (NodeCommand.Command -> m ()) -> IO AppEvent -> m ()
 eventProcessor submit nextEvent = go
@@ -188,22 +194,50 @@ eventProcessor submit nextEvent = go
             IssueFanout -> submit NodeCommand.Fanout >> go
             InitHead period -> submit (NodeCommand.Init period) >> go
             CommitToHead utxoToCommit -> submit (NodeCommand.Commit utxoToCommit) >> go
-            Bet _txIn _pp -> liftIO (putStrLn "no betting yet") >> go
+            Bet txIn txInLovelace pp -> do
+              state <- ask
+              let unsignedTx = either error id $ buildBetTx txIn txInLovelace state pp
+                  signedTx = mkSignedTx state unsignedTx
+              submit (NodeCommand.newTx signedTx)
+              go
             Claim collateralTxIn cp -> do
               state <- ask
               let unsignedTx = either error id $ buildClaimTx collateralTxIn state cp
-                  signedTx = signTx state.hsUserCredentials.userSkey unsignedTx
+                  signedTx = mkSignedTx state unsignedTx
               submit (NodeCommand.newTx signedTx) >> go
+
+mkSignedTx :: HeadState -> TxBody AlonzoEra -> Tx AlonzoEra
+mkSignedTx state = signTx state.hsUserCredentials.userSkey
 
 betConstant :: Lovelace
 betConstant = 10000000
 
+gameDatum :: UserInput.PlayParams -> Ledger.PubKeyHash -> GameDatum
+gameDatum playParams pkh = GameDatum (encryptGesture playParams.ppGesture playParams.ppSalt) pkh
+
+-- precondition: txInLovelace >= betConstant
+buildBetTx :: TxIn -> Lovelace -> HeadState -> UserInput.PlayParams -> Either String (TxBody AlonzoEra)
+buildBetTx txIn txInLovelace state playParams = do
+  let changeAddress = state.hsUserCredentials.userAddress
+      datum = gameDatum playParams state.hsUserCredentials.userPubKeyHash
+  scriptOut <- first (("bad address specifier: " <>) . show) $
+    txOutToScript state.hsNetworkId rpsValidatorAddress betConstant (TxDatum datum)
+  let changeOut
+        | txInLovelace > betConstant = [txOutToAddress changeAddress (txInLovelace - betConstant)]
+        | otherwise = []
+      bodyContent =
+        baseBodyContent
+          { txIns = [txInForSpending txIn]
+          , txOuts = scriptOut : changeOut
+          }
+  first (("bad tx-body: " <>) . show) $ makeTransactionBody bodyContent
+
 buildClaimTx :: TxIn -> HeadState -> UserInput.ClaimParams -> Either String (TxBody AlonzoEra)
 buildClaimTx collateralTxIn state cp = do
   let myTxIn = cp.myInput.txIn
-      myDatum = GameDatum (encryptGesture cp.myInput.playParams.ppGesture cp.myInput.playParams.ppSalt) cp.myInput.pkh
+      myDatum = gameDatum cp.myInput.playParams cp.myInput.pkh
       theirTxIn = cp.theirInput.txIn
-      theirDatum = GameDatum (encryptGesture cp.theirInput.playParams.ppGesture cp.theirInput.playParams.ppSalt) cp.theirInput.pkh
+      theirDatum = gameDatum cp.theirInput.playParams cp.theirInput.pkh
       redeemer = GameRedeemer (cp.myInput.pkh, cp.myInput.playParams.ppSalt) (cp.theirInput.pkh, cp.theirInput.playParams.ppSalt)
       maxTxExUnits = fromMaybe ExecutionUnits {executionSteps = 0, executionMemory = 0} $
         protocolParamMaxTxExUnits state.hsProtocolParams
