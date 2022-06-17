f94fb09565f3606551636a6653f8ec8412211d72
Giovanni Garufi <nazrhom@gmail.com>
2022-06-17 11:12:44 +0200

    tmp: working cli tests
    Change-type: patch
    Signed-off-by: Giovanni Garufi <giovanni@mlabs.city>

--- test/EndToEnd/Spec.hs
+++ test/EndToEnd/Spec.hs
@@ -1,6 +1,6 @@
 
 {-# LANGUAGE TypeApplications #-}
-{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -w #-}
 
 module EndToEnd.Spec (spec, headTests) where
 
@@ -19,8 +19,9 @@ import CardanoCluster (
  )
 import CardanoNode (RunningNode (RunningNode), newNodeConfig, withBFTNode)
 import Control.Lens ((^?))
-import Data.Aeson (Result (..), Value (Object, String), fromJSON, object, (.=))
+import Data.Aeson (Result (..), Value (Object, String), fromJSON, object, (.=), eitherDecodeStrict')
 import Data.Aeson.Lens (key)
+import Data.Text (pack)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Hydra.Cardano.Api (
@@ -35,6 +36,7 @@ import Hydra.Cardano.Api (
   mkVkAddress,
   serialiseAddress,
   unSlotNo,
+  ProtocolParameters
  )
 import Hydra.Chain.Direct.Handlers (closeGraceTime)
 import Hydra.Crypto (deriveVerificationKey, generateSigningKey)
@@ -56,10 +58,17 @@ import HydraNode (
   waitMatch,
   withHydraCluster,
   withHydraNode,
+  connection,
+  withNewClient
  )
 import Test.QuickCheck (generate)
 import Test.Tasty (TestTree)
 import Test.Tasty.Hspec (testSpec)
+import Hydra.Cluster.Util (readConfigFile)
+import HydraRPS.App (UserCommand (..), AppEvent (..), withApiClient, HeadState (..), mkUserCredentials)
+import HydraRPS.UserInput (PlayParams (..))
+import HydraRPS.OnChain (Gesture (..))
+import Control.Concurrent (writeChan)
 import qualified Prelude
 
 allNodeIds :: [Int]
@@ -85,14 +94,23 @@ spec = around showLogsOnFailure $ do
       bob = deriveParty bobSk
       carol = deriveParty carolSk
 
-  describe "End-to-end test using a single cardano-node" $ do
-    describe "three hydra nodes scenario" $
-      it "inits a Head, processes a single Cardano transaction and closes it again" $ \tracer ->
+  -- describe "End-to-end test using a single cardano-node" $ do
+  --   describe "three hydra nodes scenario" $
+  --     it "inits a Head, processes a single Cardano transaction and closes it again" $ \tracer ->
+  --       failAfter 60 $
+  --         withTempDir "end-to-end-cardano-node" $ \tmpDir -> do
+  --           config <- newNodeConfig tmpDir
+  --           withBFTNode (contramap FromCardanoNode tracer) config $ \node -> do
+  --             initAndClose tracer node
+
+  describe "RPS tests" $ do
+    describe "place and observe bet" $
+      it "inits a Head, places a bet then closes the head" $ \tracer ->
         failAfter 60 $
           withTempDir "end-to-end-cardano-node" $ \tmpDir -> do
             config <- newNodeConfig tmpDir
             withBFTNode (contramap FromCardanoNode tracer) config $ \node -> do
-              initAndClose tracer node
+              initBetAndClose tracer node
 
   
 initAndClose :: Tracer IO EndToEndLog -> RunningNode -> IO ()
@@ -102,9 +120,9 @@ initAndClose tracer node@(RunningNode _ nodeSocket) = do
     bobKeys@(bobCardanoVk, _) <- generate genKeyPair
     carolKeys@(carolCardanoVk, _) <- generate genKeyPair
 
-    let aliceSk = generateSigningKey ("alice")
-    let bobSk = generateSigningKey ("bob")
-    let carolSk = generateSigningKey ("carol")
+    let aliceSk = generateSigningKey "alice"
+    let bobSk = generateSigningKey "bob"
+    let carolSk = generateSigningKey "carol"
 
     let alice = deriveParty aliceSk
     let bob = deriveParty bobSk
@@ -208,6 +226,72 @@ initAndClose tracer node@(RunningNode _ nodeSocket) = do
           failure $ "newUTxO isn't valid JSON?: " <> err
         Success u ->
           failAfter 5 $ waitForUTxO defaultNetworkId nodeSocket u
+
+
+initBetAndClose :: Tracer IO EndToEndLog -> RunningNode -> IO ()
+initBetAndClose tracer node@(RunningNode _ nodeSocket) = do
+  withTempDir "end-to-end-init-bet-and-close" $ \tmpDir -> do
+    aliceKeys@(aliceCardanoVk, aliceCardanoSk) <- generate genKeyPair
+    bobKeys@(bobCardanoVk, bobCardanoSk) <- generate genKeyPair
+
+    let aliceSk = generateSigningKey "alice"
+    let bobSk = generateSigningKey "bob"
+
+    let alice = deriveParty aliceSk
+    let bob = deriveParty bobSk
+
+    let cardanoKeys = [aliceKeys, bobKeys]
+        hydraKeys = [aliceSk, bobSk]
+
+    let firstNodeId = 0
+
+    withHydraCluster tracer tmpDir nodeSocket firstNodeId cardanoKeys hydraKeys $ \nodes -> do
+      let [aliceNode, bobNode] = toList nodes
+      waitForNodesConnected tracer [aliceNode, bobNode]
+      putStrLn "\nnodes are up"
+      -- nodes need to be up to observe fuel tx
+      -- hydra node port is 5000 + id
+      -- Funds to be used as fuel by Hydra protocol transactions
+      seedFromFaucet_ defaultNetworkId node aliceCardanoVk 100_000_000 Fuel
+      seedFromFaucet_ defaultNetworkId node bobCardanoVk 100_000_000 Fuel
+
+      let contestationPeriod = 2 :: Int
+      pp <- eitherDecodeStrict' <$> readConfigFile "protocol-parameters.json" :: IO (Either String ProtocolParameters)
+      let protocolParams = either (error . pack) id pp
+          aliceCredentials = mkUserCredentials defaultNetworkId aliceCardanoSk
+          bobCredentials = mkUserCredentials defaultNetworkId bobCardanoSk
+          aliceState = HeadState aliceCredentials defaultNetworkId protocolParams
+          bobState = HeadState bobCredentials defaultNetworkId protocolParams
+      putStrLn "starting client"
+      withApiClient aliceState "127.0.0.1" 4000 $ \enqueueAliceCommand' -> do
+        withApiClient bobState "127.0.0.1" 4001 $ \enqueueBobCommand' -> do
+          putStrLn "api client ready \n"
+          let enqueueAliceCommand = do
+                threadDelay 500
+                enqueueAliceCommand'
+          let enqueueBobCommand = do
+                threadDelay 500
+                enqueueBobCommand'
+          enqueueAliceCommand $ InitHead contestationPeriod
+
+          -- send aliceNode $ input "Init" ["contestationPeriod" .= contestationPeriod]
+          -- newBobNode <- withNewClient bobNode pure
+          waitFor tracer 10 [aliceNode, bobNode] $
+            output "ReadyToCommit" ["parties" .= Set.fromList [alice, bob]]
+
+          -- Get some UTXOs to commit to a head
+          committedUTxOByAlice <- seedFromFaucet defaultNetworkId node aliceCardanoVk aliceCommittedToHead Normal
+          committedUTxOByBob <- seedFromFaucet defaultNetworkId node bobCardanoVk bobCommittedToHead Normal
+          enqueueAliceCommand $ CommitToHead (UTxO.toApi committedUTxOByAlice)
+          enqueueBobCommand $ CommitToHead (UTxO.toApi  committedUTxOByBob)
+
+          waitFor tracer 20 [aliceNode, bobNode] $ output "HeadIsOpen" ["utxo" .= (committedUTxOByAlice <> committedUTxOByBob)]
+
+          enqueueAliceCommand $ Bet $ PlayParams { ppGesture = Rock, ppSalt = "1234" }
+
+          waitFor tracer 10 [aliceNode, bobNode] $ output "HeadIsOpena" ["utxo" .= (committedUTxOByAlice <> committedUTxOByBob)]
+          pure ()
+
 
 --
 -- Fixtures
